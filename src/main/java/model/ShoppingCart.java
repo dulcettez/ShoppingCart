554e81dbce69175e02e13aceaf51c84d9ebc4cd8
Haton Alkhaldi <122685128+dulcettez@users.noreply.github.com>
2025-06-02 08:55:30 +0300

    Update ShoppingCart.java

--- src/main/java/model/ShoppingCart.java
+++ src/main/java/model/ShoppingCart.java
@@ -4,7 +4,7 @@ import jakarta.persistence.*;
 import java.util.*;
 
 @Entity
-@Table(name = "yourname_cart")
+@Table(name = "haton_alkhaldi_cart")
 public class ShoppingCart {
     @Id
     private String id; // use UUID
